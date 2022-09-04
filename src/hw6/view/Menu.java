8aaf65f51cdf49cc994689eaa45fe80329465f21
zizou <zedesm23@gmail.com>
2022-09-05 01:18:10 +0430

    Some Changes

--- src/hw6/view/Menu.java
+++ src/hw6/view/Menu.java
@@ -4,14 +4,18 @@ import hw6.entity.Team;
 import hw6.repository.FootballRepository;
 
 import java.sql.SQLException;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Menu {
-    private static String[] options = {"1- Add Club", "2- Delete Club", "3- Add Game Between 2 Clubs", "4- View Information About Club", "5- View Table Of Composition"};
-    private static Team team = new Team();
-    private static Team teamH = new Team();
-    private static FootballRepository footballRepository = new FootballRepository();
-    private static int points, played, won, drawn, lose;
+    private static final String[] options = {"1- Add Club", "2- Delete Club", "3- Add Game Between 2 Clubs", "4- View Information About Club", "5- View Table Of Composition"};
+    private static final FootballRepository footballRepository = new FootballRepository();
+    private static int played;
+    private static int won;
+    private static int drawn;
+    private static int lose;
+    private static int drawn2;
+    private static ClubComparator clubComparator ;
 
     public static void printMenu(String[] options) {
         for (String option : options) {
@@ -24,13 +28,12 @@ public class Menu {
 
     public static void main(String[] args) throws Exception {
         printMenu(options);
-        int play = 0;
         int option = scanner.nextInt();
         switch (option) {
             case 1:
                 System.out.println("Enter Name Of Club:");
                 String clubName = scanner.next();
-                team = new Team(clubName);
+                Team team = new Team(clubName);
                 setTeam(team.getClub());
                 break;
             case 2:
@@ -45,6 +48,8 @@ public class Menu {
                 System.out.println("Enter Name Of Club:");
                 String name = scanner.next();
                 System.out.println(findName(name));
+                break;
+            case 5:
 
         }
 
@@ -100,6 +105,93 @@ public class Menu {
                 int lost = secondTeam.getLost();
                 lose += lost;
                 secondTeam.setLost(lose);
+            } else if (gft1 == gft2) {
+                //setgf
+                int gf1 = firstTeam.getGf();
+                gf1 += gft1;
+                firstTeam.setGf(gf1);
+                int gf2 = secondTeam.getGf();
+                gf2 += gft2;
+                secondTeam.setGf(gf2);
+                //setplayed
+                int played1 = firstTeam.getPlayed();
+                int played2 = secondTeam.getPlayed();
+                played++;
+                played1 += played;
+                played2 += played;
+                firstTeam.setPlayed(played1);
+                secondTeam.setPlayed(played2);
+                //setga
+                int ga1 = firstTeam.getGa();
+                ga1 += gft2;
+                firstTeam.setGa(ga1);
+                int ga2 = secondTeam.getGa();
+                ga2 += gft1;
+                secondTeam.setGa(ga2);
+                //setgd
+                int gd1 = gf1 - ga1;
+                firstTeam.setGd(gd1);
+                int gd2 = gf2 - ga2;
+                secondTeam.setGd(gd2);
+                //setpoints
+                drawn++;
+                drawn2++;
+                int points = firstTeam.getPoints();
+                points += drawn;
+                firstTeam.setPoints(points);
+                int points2 = secondTeam.getPoints();
+                points2 += drawn2;
+                secondTeam.setPoints(points2);
+                //setdrawn1
+                int dw1 = firstTeam.getDrawn();
+                drawn += dw1;
+                firstTeam.setDrawn(drawn);
+                //setdrawn2
+                int dw2 = secondTeam.getDrawn();
+                drawn += dw2;
+                secondTeam.setDrawn(drawn);
+            } else {
+                //setgf
+                int gf1 = firstTeam.getGf();
+                gf1 += gft1;
+                firstTeam.setGf(gf1);
+                int gf2 = secondTeam.getGf();
+                gf2 += gft2;
+                secondTeam.setGf(gf2);
+                //setplayed
+                int played1 = firstTeam.getPlayed();
+                int played2 = secondTeam.getPlayed();
+                played++;
+                played1 += played;
+                played2 += played;
+                firstTeam.setPlayed(played1);
+                secondTeam.setPlayed(played2);
+                //setga
+                int ga1 = firstTeam.getGa();
+                ga1 += gft2;
+                firstTeam.setGa(ga1);
+                int ga2 = secondTeam.getGa();
+                ga2 += gft1;
+                secondTeam.setGa(ga2);
+                //setgd
+                int gd1 = gf1 - ga1;
+                firstTeam.setGd(gd1);
+                int gd2 = gf2 - ga2;
+                secondTeam.setGd(gd2);
+                won++;
+                lose++;
+                //setpoints
+                int points = secondTeam.getPoints();
+                points += won * 3;
+                secondTeam.setPoints(points);
+                //setwon
+                int win = secondTeam.getWon();
+                won += win;
+                secondTeam.setWon(won);
+                //setlost
+                int lost = firstTeam.getLost();
+                lose += lost;
+                firstTeam.setLost(lose);
             }
             updateByName(firstTeam);
             updateByName(secondTeam);
@@ -111,12 +203,12 @@ public class Menu {
 
     }
 
-    public static boolean setTeam(String teamName) throws SQLException {
-        return footballRepository.addClub(teamName);
+    public static void setTeam(String teamName) throws SQLException {
+        footballRepository.addClub(teamName);
     }
 
     public static Team findName(String name1) throws SQLException {
-        Team teams = null;
+        Team teams;
         try {
             teams = footballRepository.findByName(name1);
             return teams;
@@ -125,5 +217,12 @@ public class Menu {
             System.out.println("Team Not Found !");
         }
         return null;
+    }
+    public class ClubComparator implements Comparator<Team> {
+
+        @Override
+        public int compare(Team obj1, Team obj2) {
+            return Integer.valueOf(obj1.getPoints()).compareTo(Integer.valueOf(obj2.getPoints()))  ;
+        }
     }
 }
